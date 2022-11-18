385a5d66d6ef918743f6f238b81b0b4fa464a328
ryeojin <ryeojin@biztalk.co.kr>
2022-11-18 10:51:50 +0900

    HomeController 테스트, 빌드 후 애플리케이션 실행

--- Chapter01/taco-cloud/src/main/java/tacos/TacoCloudApplication.java
+++ Chapter01/taco-cloud/src/main/java/tacos/TacoCloudApplication.java
@@ -5,9 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TacoCloudApplication {
+    /* @SpringBootApplication
+    => @SpringBootConfiguration + @EnableAutoConfiguration + @ComponentScan
 
-    public static void main(String[] args) {
-        SpringApplication.run(TacoCloudApplication.class, args);
-    }
+    @SpringBootConfiguration
+    : 현재 클래스를 구성 클래스로 지정.
 
+    @EnableAutoConfiguration
+    : 스프링 부트 자동-구성 활성화
+
+    @ComponentScan
+    : 컴포넌트 검색 활성화 (@Component, @Controller, @Service)
+     */
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+    /* main() => 실제로 애플리케이션을 시작시키고 스프링 애플리케이션 컨텍스트를 생성하는 SpringApplication 의 run() 메서드 호출.
+    run() 에 전달되는 두 개의 매개변수는 구성 클래스와 명령행(command-line) 인자다.
+     */
 }
--- Chapter01/taco-cloud/src/test/java/tacos/TacoCloudApplicationTests.java
+++ Chapter01/taco-cloud/src/test/java/tacos/TacoCloudApplicationTests.java
@@ -3,7 +3,7 @@ package tacos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest // 스프링 애플리케이션 컨텍스트가 성공적으로 로드될 수 있는지 검사
 class TacoCloudApplicationTests {
 
     @Test
