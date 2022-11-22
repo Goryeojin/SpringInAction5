0216bf632f32b72cb484046a72b9c361d4a9caea
goryeojin <sooon9254@naver.com>
2022-11-22 17:22:17 +0900

    JDBCTemplate, SimpleJdbcInsert 사용하여 H2에 주문한 타코 저장

--- Chapter03/taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ Chapter03/taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -5,13 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
+import tacos.Order;
 import tacos.Taco;
 import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -25,14 +25,27 @@ import java.util.stream.Collectors;
  */
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
 
 	private final IngredientRepository ingredientRepo;
+    private TacoRepository tacoRepo;
 
 	@Autowired
-	public DesignTacoController(IngredientRepository ingredientRepo) {
+	public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository tacoRepo) {
 		this.ingredientRepo = ingredientRepo;
+        this.tacoRepo = tacoRepo;
 	}
+
+    @ModelAttribute(name = "order")
+    public Order order() {
+        return new Order();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
 
 	@GetMapping
 	public String showDesignForm(Model model) {
@@ -69,11 +82,14 @@ public class DesignTacoController {
 	}
 
 	@PostMapping
-	public String processDesign(@Valid Taco design, Errors errors) {
+	public String processDesign(@Valid Taco design, Errors errors, @ModelAttribute Order order) {
 		if (errors.hasErrors()) {
 			return "design";
 		}
 		log.info("Processing design: " + design);
+
+        Taco saved = tacoRepo.save(design);
+        order.addDesign(saved);
 
 		return "redirect:/orders/current";
 		// redirect: => 상대 경로로 재접속
